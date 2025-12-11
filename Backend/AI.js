5469597a46342c621fe94cb2a2317e0d05dfdcca
Vincent <nwolisavincent@gmail.com>
2025-12-11 23:19:54 +0100

    Added the embedded AI

---
+++ Backend/AI.js
@@ -0,0 +1,39 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import readline from "readline";
+import dotenv from "dotenv";
+dotenv.config();
+
+const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+async function run() {
+  const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
+
+  const chat = model.startChat({
+    history: [],
+    generationConfig: {
+      maxOutputTokens: 1024,
+    },
+  });
+
+  async function askAndRespond() {
+    rl.question("You: ", async (msg) => {
+      if (msg.toLowerCase() === "exit") {
+        rl.close();
+      } else {
+        const result = await chat.sendMessage(msg);
+        const response = await result.response;
+        const text = await response.text();
+        console.log("AI: " + text);
+        askAndRespond();
+      }
+    });
+  }
+
+  askAndRespond();
+}
+run();
