08cee505d559deee482eca5d101dc42502d0e6a2
Vincent <nwolisavincent@gmail.com>
2025-12-12 00:10:41 +0100

    slight adjustment

--- Backend/AI.js
+++ Backend/AI.js
@@ -1,6 +1,7 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import readline from "readline";
 import dotenv from "dotenv";
+
 dotenv.config();
 
 const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
